d0dc2a5c4aed97a1cbd711e7e7f3a0f35bec1538
yanshuman yadav <90574468+yanshuy@users.noreply.github.com>
2025-03-03 10:41:37 +0000

    therst

---
+++ go.mod
@@ -0,0 +1,3 @@
+module yanshuy/play
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+type Server struct{
+	listenAddr string
+	ln net.Listener
+}
+
+func NewServer(addr string) *Server{
+	return &Server{
+		listenAddr: addr,
+	}
+}
+
+func (s *Server) Start() error{
+	ln, err := net.Dial("tcp", s.listenAddr)
+	if err != nil {
+		log.Fatal("Could not Start the server")
+	}
+}
+
+func main(){
+
+}
